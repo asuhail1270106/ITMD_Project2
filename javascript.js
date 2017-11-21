ccd9ac363bbc7601f8257db7564bcd668bf0971c
mherna18 <marcohernandez29@gmail.com>
2017-11-21 17:30:26 -0600

    added functions add/delete

--- javascript.js
+++ javascript.js
@@ -11,10 +11,48 @@ function emptyFields() {
     }
 }
 
-function add_class(n)
-{
-    var tr = n.parentNode.parentNode.cloneNode(true);
-    document.getElementById('classes').appendChild(tr);
+function add_row(tableID) {
+  var table = document.getElementById(tableID);
+  var rowCount = table.rows.length;
+  var row = table.insertRow(rowCount);
+  #cell 1
+  var cell1 = row.insertCell(0);
+  var element1 = document.createElement("input");
+  element1.type = "checkbox";
+  element1.name="chkbox[]";
+  cell1.appendChild(element1);
+  #cell 2
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  var element2 = document.createElement("input");
+  element2.type = "text";
+  element2.name = "txtbox[]";
+  cell3.appendChild(element2);
+  #cell 3
+  var cell4 = row.insertCell(3);
+  var element2 = document.createElement("input");
+  element3.type = "text";
+  element3.name = "txtbox[]";
+  element3.name = "txtbox[]";
+  cell4.appendChild(element3);
+}
+
+function delete_row(tableID) {
+try {
+  var table = document.getElementById(tableID);
+  var rowCount = table.rows.length;
+for(var i=0; i<rowCount; i++) {
+  var row = table.rows[i];
+  var chkbox = row.cells[0].childNodes[0];
+if(null != chkbox && true == chkbox.checked) {
+  table.deleteRow(i);
+  rowCount--;
+  i--;
+    }
+  }
+}catch(e) {
+    alert(e);
+  }
 }
 
 $(document).ready(function(){
